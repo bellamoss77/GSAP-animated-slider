30327f7c9f262bb59885071ade75ca0b6f7efe13
bellamoss77 <mmf295@icloud.com>
2024-05-13 00:59:07 -0400

    add HTML, SCSS, start DOM.js

---
+++ JS/DOM.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.slideshow-container');
+    const nextBtn = document.querySelector('.nav-next');
+    const prevBtn = document.querySelector('.nav-prev');
+    const images = [
+        {
+            
+        }
+    ]
+})
